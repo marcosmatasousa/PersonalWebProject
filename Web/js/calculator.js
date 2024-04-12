40dae2c337fb2498ae2b4a83bde629203a70ec3f
marcosmatasousa <marcosdamata2000@gmail.com>
2024-04-12 13:52:37 -0300

    better implementation on the calculator

--- Web/js/calculator.js
+++ Web/js/calculator.js
@@ -1,70 +1,26 @@
 display = document.getElementById("display");
-numbers = [];
-aux = "";
-
 
 function calculate(){
-    var flag = "";
-    var currentValue = 0;
-    if(typeof numbers[0] == 'number'){
-        currentValue += numbers[0]
-    }
-    for(let i = 1; i <= numbers.length - 1; i++){
-        if(typeof numbers[i] === 'string'){
-            flag = numbers[i]
-        }
-        else{
-            if (flag === '+'){
-                currentValue += numbers[i];
-            }
-            else if (flag === '-'){
-                currentValue -= numbers[i];
-            }
-            else if (flag === '*'){
-                currentValue *= numbers[i];
-            }
-            else{
-                currentValue /= numbers[i]
-            }
-        }
-    }
-    display.innerHTML = currentValue;
-    numbers = [];
-    aux = currentValue;
+    const result = eval(display.innerHTML);
+    display.innerHTML = result;
 }
 
 function updateDisplay(item){
+    // Appends every entry to the expr till the user press the equal button
     if (item === '='){
-        numbers.push(parseInt(aux));
-        aux = "";
-        calculate();
+        calculate()
     }
     else{
-        if (typeof item == 'string' && item != '.'){
-            if (aux.includes('.')){
-                numbers.push(parseFloat(aux));
-            }
-            else{
-                numbers.push(parseInt(aux));
-            }
-            numbers.push(item);
-            aux = "";
-        }
-        else{
-            aux += item;
-        }
-        
-        display.innerHTML += item;
+        display.innerHTML += item
     }
 }
 
 function C(){
-    numbers = [];
-    aux = "";
+    // Cleans the display
     display.innerHTML = "";
 }
 
 function CE(){
+    // Deletes de last user entry
     display.innerHTML = display.innerHTML.slice(0, -1);
-    aux = display.innerHTML;
 }
